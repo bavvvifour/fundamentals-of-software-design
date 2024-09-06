72ae9cd020bc18e3d3ff67e58f280910736f2eaf
bavvvifour <bayanovv8091@gmail.com>
2024-09-06 21:50:11 +0700

    init

---
+++ music-service-api/src/main/java/sfu/musicserviceapi/MusicServiceApiApplication.java
@@ -0,0 +1,13 @@
+package sfu.musicserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicServiceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicServiceApiApplication.class, args);
+    }
+
+}
