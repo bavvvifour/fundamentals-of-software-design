ecdf95adf6ee8c780374d48ffbc851a49b3a4f4f
bavvvifour <bayanovv8091@gmail.com>
2024-09-12 13:20:27 +0700

    add Dockerfile

--- telegramBot/src/bitcoin_service.py
+++ telegramBot/src/bitcoin_service.py
@@ -4,11 +4,10 @@ from requests.exceptions import HTTPError
 
 def get_bitcoin_data():
     try:
-        response = requests.get('http://localhost:8080/api/bitcoin')
+        response = requests.get('http://first-api-service:8080/api/bitcoin')
         response.raise_for_status()
-        jsonResponse = response.json()
 
-        return jsonResponse
+        return response.json()
 
     except HTTPError as http_err:
         print(f'HTTP error occurred: {http_err}')
